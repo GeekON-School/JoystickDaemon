1393be6b5380cb42d37c30d9bcb8ae2f2ead2be1
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2020-01-25 12:53:31 +0300

    Add debug.py

---
+++ debug.py
@@ -0,0 +1,13 @@
+import pygame
+import pyautogui
+
+
+def main():
+    pygame.init()
+    pygame.joystick.init()
+    joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+    print(joysticks)
+
+
+if __name__ == '__main__':
+    main()
