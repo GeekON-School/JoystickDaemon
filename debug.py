b148a99521636a4c5dc9dcc525c90b5291a27705
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2020-01-25 13:04:52 +0300

    Joystick Info

--- debug.py
+++ debug.py
@@ -6,7 +6,13 @@ def main():
     pygame.init()
     pygame.joystick.init()
     joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-    print(joysticks)
+    for j in joysticks:
+        j.init()
+        print(j.get_name())
+        print("  Balls:", j.get_numballs())
+        print("  Axes:", j.get_numaxes())
+        print("  Hats:", j.get_numhats())
+        print("  Buttons:", j.get_numbuttons())
 
 
 if __name__ == '__main__':
