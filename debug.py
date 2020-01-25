8858b12304dd4b6732a02e13847043576ce016ea
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2020-01-25 13:19:17 +0300

    Print buttons

--- debug.py
+++ debug.py
@@ -1,5 +1,6 @@
 import pygame
 import pyautogui
+import time
 
 
 def main():
@@ -9,10 +10,17 @@ def main():
     for j in joysticks:
         j.init()
         print(j.get_name())
-        print("  Balls:", j.get_numballs())
-        print("  Axes:", j.get_numaxes())
-        print("  Hats:", j.get_numhats())
-        print("  Buttons:", j.get_numbuttons())
+        print("  Balls: {}".format(j.get_numballs()))
+        print("  Axes: {}".format(j.get_numaxes()))
+        print("  Hats: {}".format(j.get_numhats()))
+        print("  Buttons: {}".format(j.get_numbuttons()))
+
+    while True:
+        for i, j in enumerate(joysticks):
+            print("j{} ".format(i) + "".join("{: 3}".format('#' if j.get_button(x) else 'O')
+                                             for x in range(j.num_buttons())))
+            print("   " + "".join("{: 3}".format(x) for x in range(j.num_buttons())))
+        time.sleep(1)
 
 
 if __name__ == '__main__':
